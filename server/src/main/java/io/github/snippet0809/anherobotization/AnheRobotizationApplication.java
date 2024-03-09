79192f0ea1da4d5a189d5d3bd8fa3898a4ca61a8
Spet <snippet0809@163.com>
2024-03-09 18:49:34 +0800

    first commit

---
+++ server/src/main/java/io/github/snippet0809/anherobotization/AnheRobotizationApplication.java
@@ -0,0 +1,13 @@
+package io.github.snippet0809.anherobotization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnheRobotizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnheRobotizationApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/github/snippet0809/anherobotization/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.snippet0809.anherobotization.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
